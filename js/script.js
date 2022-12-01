d126032daad7226da06d83fcb8b87918e3999d31
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-12-01 16:22:22 +0100

    Added other properties to the work in js

--- js/script.js
+++ js/script.js
@@ -17,12 +17,47 @@ CODICE PRINCIPALE */
 // * EVENTI
 playBtn.addEventListener('click' , function(){
     if(mainElement.innerText === ''){
-        // ? CREAZIONE E INSERIMENTO NEL DOM DEL CONTENITORE GRIGLIA
+        // ? DICHIARAZIONI E INIZIALIZZAZIONI INIZIALI
+        let numberOfBombs = 16;
+        const elemenstWithBomb = [];
+        let score = 0;
+
+
+        // ? INSIMENTO DELLE INFO DELLA PARTITA NEL DOM
+
+        // * Creazione delle costanti
+        const infoMatch = getAnElement('div','info');
+        const scoreContainer = getAnElement('span' , 'score-number');
+        const numberOfBombsContainer = getAnElement('span','bombs-number')
+
+        // * Inserimento del contenuto
+        scoreContainer.innerHTML = 'Punteggio: 0';
+        numberOfBombsContainer.innerHTML = `Bombe in partita:${numberOfBombs};`
+
+        // * Inserimento nel DOM
+        infoMatch.append(scoreContainer);
+        scoreContainer.after(numberOfBombsContainer);
+        mainElement.append(infoMatch);
+
+
+        // ? CAMBIAMENTO DEL CONTENUTO DEL BOTTONE
+        playBtn.textContent ='Restart';
+
+
+        // ? CREAZIONE E INSERIMENTO NEL DOM DELLA GRIGLIA
         const gridContainer = getAnElement('div','my-grid d-flex flex-wrap align-items-center');
         
         mainElement.append(gridContainer);
 
         
+        // ? CREAZIONE DELLE BOMBE
+        for(let i=0 ; i<numberOfBombs ; i++){
+            let elementsOfTheListOfBombs = getARandomNumber(1 , 100);
+            elemenstWithBomb.push(elementsOfTheListOfBombs);
+        }
+        console.log(elemenstWithBomb)
+
+
         // ? CREAZIONE DI UN SINGOLO ELEMENTO DELLA GRIGLIA
         for(let i=1 ; i<=100 ; i++){
             // * Creazione e inserimento del singolo elemento della griglia
@@ -36,13 +71,22 @@ playBtn.addEventListener('click' , function(){
             gridSingleElement.addEventListener('click' , function(){
                 console.log(i);
 
-                gridSingleElement.classList.toggle('active');
+                gridSingleElement.classList.add('my-active');
+
+                if(elemenstWithBomb.includes(i)){
+                    alert('BOOOOOM!\nHai perso deficiente.');
+                }else if(!gridSingleElement.classList.contains('my-active')){
+                    score++;
+                    scoreContainer.innerHTML = `Punteggio:${score}`;
+                    console.log(score);
+                }
             })
         }
     }
     
     else{
         mainElement.firstElementChild.remove();
+        playBtn.textContent = 'Play'
     }
 })
 
@@ -60,4 +104,12 @@ function getAnElement(element , elementClasses){
     htmlElement.className = elementClasses;
 
     return htmlElement;
-} 
+}
+
+
+
+// * FUNZIONE PER GENERARE UN NUMERO CASUALE
+function getARandomNumber(minValue , maxValue){
+    const randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+    return randomNumber;
+}
